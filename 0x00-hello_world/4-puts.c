bc1949d3c00a081c8993fcda177f736368c0e468
moyino <chemesnalmuyi04@gmail.com>
2022-10-15 07:58:11 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * * main - Entry point
- * *
- * *Description:'Using put command to print an output'
- * *
- * * Return: Always 0 (Success)
- * */
+ * main - Entry point
+ *
+ *Description:'Using put command to print an output'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * * main - Prints the sizeof many var types
- * *
- * * Return: Always 0 (Success)
- * */
+ * main - Prints the sizeof many var types
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char a;
@@ -11,6 +11,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
