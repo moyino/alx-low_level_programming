9b1ab07b77f86b8d78e62481a3f249c20b294226
moyino <chemesnalmuyi04@gmail.com>
2022-10-15 07:14:52 -0700

    low level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * * main - Entry point
+ * *
+ * *Description:'Using put command to print an output'
+ * *
+ * * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
